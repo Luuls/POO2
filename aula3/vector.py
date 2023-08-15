25b6a35a6e1756237a8cce74c37a04f841525a33
Luuls <luan13467@hotmail.com>
2023-08-15 10:44:23 -0300

    fiz uma classe para modelar um vetor. Não terminei

---
+++ aula3/vector.py
@@ -0,0 +1,41 @@
+class Vector:
+    def __init__(self, *elements):
+        self.__elements = list(elements)
+        self.__size = len(self.__elements)
+
+    @property
+    def elements(self):
+        return self.__elements
+
+    @elements.setter
+    def elements(self, new_elements):
+        self.__elements = new_elements
+        self.__size = len(self.__elements)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @classmethod
+    def from_list(cls, elements):
+        return cls(*elements)
+
+    def product_by_scalar(self, value):
+        for i in range(self.__size):
+            self.__elements[i] *= value
+
+    def __add__(self, other):
+        if self.__size != other.size:
+            raise ValueError
+
+        new_vector = [0] * self.__size
+        for i, (this_element, other_element) in enumerate(zip(self.__elements, other.elements)):
+            new_vector[i] = this_element + other_element
+
+        return Vector.from_list(new_vector)
+
+v = Vector.from_list([1, 2, 3])
+print(v.elements)
+v2 = Vector(4, 5, 6)
+print(v2.elements)
+print(f'{v.elements} + {v2.elements} = {(v + v2).elements}')
