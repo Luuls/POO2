0348e6c055a8922c9fd3324ae1b678d29beadb19
Luuls <luan13467@hotmail.com>
2023-08-20 01:32:42 -0300

    implementada soma e subtração entre polinômios. Também implementado método para multiplicar o polinômio por uma constante

--- lista1/polynomial.py
+++ lista1/polynomial.py
@@ -3,6 +3,12 @@ class Monomial:
         self.__degree = degree
         self.__coefficient = coefficient
 
+    def calculate(self, input: int):
+        return self.__coefficient * input ** self.__degree
+
+    def multiply_by_constant(self, value):
+        self.__coefficient *= value
+
     def get_degree(self):
         return self.__degree
 
@@ -15,16 +21,92 @@ class Monomial:
     def set_coefficient(self, value: int):
         self.__coefficient = value
 
+    def __add__(self, other: 'Monomial'):
+        return Monomial(self.__degree, self.__coefficient + other.get_coefficient())
+
+    def __str__(self) -> str:
+        return f'{self.__coefficient}x^{self.__degree}'
+
+    def __repr__(self) -> str:
+        return str(self)
+
 
 class Polynomial:
     def __init__(self, terms: list[Monomial]):
-        self.__terms = terms
+        if len(terms) == 0:
+            raise BufferError('Cannot construct a polynomial with 0 terms')
+            
+        self.__terms = sorted(terms, key=lambda term: term.get_degree())
+        self.__degree = self.__terms[-1].get_degree()
         
-    def get_terms(self):
+    def get_terms(self) -> list[Monomial]:
         return self.__terms.copy()
 
-    def set_terms(self, new_terms: list[Monomial]):
+    def set_terms(self, new_terms: list[Monomial]) -> None:
         self.__terms = new_terms
+        self.__degree = self.__terms[-1].get_degree()
+
+    def get_degree(self):
+        return self.__degree
+
+    def __add__(self, other: 'Polynomial') -> 'Polynomial':
+        self_index = other_index = 0
+        self_len = len(self.__terms)
+
+        other_terms = other.get_terms()
+        other_len = len(other_terms)
+
+        result: list[Monomial] = []
+        while self_index < self_len or other_index < other_len:
+            self_term = self.__terms[self_index]
+            other_term = other_terms[other_index]
+
+            if self_term.get_degree() == other_term.get_degree():
+                terms_sum = self_term + other_term
+                if terms_sum.get_coefficient() != 0:
+                    result.append(terms_sum)
+
+                if self_index < self_len:
+                    self_index += 1
+
+                if other_index < other_len:
+                    other_index += 1
+
+            elif self_term.get_degree() > other_term.get_degree():
+                result.append(other_term)
+                if other_index < other_len:
+                    other_index += 1
+
+            elif self_term.get_degree() < other_term.get_degree():
+                result.append(self_term)
+                if self_index < self_len:
+                    self_index += 1
+
+        return Polynomial(result)
+
+    def multiply_by_constant(self, value) -> 'Polynomial':
+        '''returns a new polynomial with terms multiplied by the passed constant'''
+        new_terms = self.__terms.copy()
+        for i, term in enumerate(self.__terms):
+            new_terms[i].multiply_by_constant(value)
+
+        return Polynomial(new_terms)
+
+    def multiply_by_constant_in_place(self, value):
+        for i, term in enumerate(self.__terms):
+            self.__terms[i].multiply_by_constant(value)
+
+    def __sub__(self, other):
+        return self.multiply_by_constant(-1) + other
+        
+    def __repr__(self) -> str:
+        return ' + '.join([str(term) for term in self.__terms])
 
 
-pol1 = Polynomial([Monomial(0), Monomial(1), Monomial(2)])
+pol1 = Polynomial([Monomial(2), Monomial(0), Monomial(1), Monomial(3), Monomial(5)])
+print(f'  {pol1}')
+
+pol2 = Polynomial([Monomial(3), Monomial(4), Monomial(5)])
+print(f'+ {pol2}')
+print(f'\n= {pol1 + pol2}')
+
