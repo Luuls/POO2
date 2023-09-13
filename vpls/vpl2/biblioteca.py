39e06d6111fddadb8b61409af3584b9298b35f50
Luuls <luan13467@hotmail.com>
2023-09-12 22:06:15 -0300

    Implementei a classe Biblioteca. Falta testar no VPL

--- vpls/vpl2/biblioteca.py
+++ vpls/vpl2/biblioteca.py
@@ -6,11 +6,38 @@ class Biblioteca:
         self.__livros = []
 
     def incluirLivro(self, livro: Livro):
-        #Nao esqueca de garantir que o objeto recebido pertence a classe Livro...
-        ...
+        if not isinstance(livro, Livro):
+            raise TypeError
+
+        try:
+            self.__procurar(self.livros, livro.codigo, chave=lambda x: x.codigo)
+
+        except ValueError:
+            self.livros.append(livro)
+            return
+
+        raise ValueError(f'O livro {livro.codigo} já está cadastrado nesta biblioteca')
 
     def excluirLivro(self, livro: Livro):
-        ...
+        if not isinstance(livro, Livro):
+            raise TypeError
+
+        try:
+            indice = self.__procurar(self.livros, livro.codigo, chave=lambda x: x.codigo)
+
+        except ValueError:
+            raise ValueError(f'O livro {livro.codigo} não está cadastrado nesta biblioteca')
+
+        self.livros.pop(indice)
+        return
+
+    # não vou criar uma função global para todas as classes usarem, pois creio que no VPL não vá funcionar
+    def __procurar(self, conteiner, valor, chave=lambda x: x):
+        for i, valorInterno in enumerate(conteiner):
+            if valor == chave(valorInterno):
+                return i
+
+        raise ValueError('Valor não encontrado no contêiner')
 
     @property
     def livros(self):
