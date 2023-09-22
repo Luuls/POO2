2f73da516c7fdc48ff061baa256c1d07f1b0d2cd
Luuls <luan13467@hotmail.com>
2023-09-22 20:53:59 -0300

    fiz o TipoChamado

--- vpls/vpl4/tipoChamado.py
+++ vpls/vpl4/tipoChamado.py
@@ -3,3 +3,21 @@ from abstractTipoChamado import AbstractTipoChamado
 
 class TipoChamado(AbstractTipoChamado):
     def __init__(self, codigo: int, descricao: str, nome: str):
+        self.__codigo = codigo
+        self.__descricao = descricao
+        self.__nome = nome
+
+    def __eq__(self, other):
+        if not isinstance(other, TipoChamado):
+            return False
+        
+        return self.codigo == other.codigo
+
+    def codigo(self) -> int:
+        return self.__codigo
+
+    def descricao(self) -> str:
+        return self.__descricao
+
+    def nome(self) -> str:
+        return self.__nome
