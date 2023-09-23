3ea52e72c9f578cc34409eb889fe3fb50e59759f
Luuls <luan13467@hotmail.com>
2023-09-23 00:29:06 -0300

    fiz o jogador

--- vpls/vpl5/Jogador.py
+++ vpls/vpl5/Jogador.py
@@ -7,14 +7,15 @@ import random
 class Jogador(AbstractJogador):
 
     def __init__(self, nome: str):
-        pass#implementar
+        self.__nome = nome
+        self.__mao = []
 
     '''
     @return Retorna o nome do jogador
     '''
     @property
     def nome(self) -> str:
-        pass#implementar
+        return self.__nome
 
     '''
     Retira uma das cartas do Jogador. Esta operacao eh utilizada para realizar uma jogada (baixar uma carta na mesa)
@@ -22,18 +23,20 @@ class Jogador(AbstractJogador):
     @return Retorna a Carta que foi retirada da mao do jogador (lista das cartas que ele possui)
     '''
     def baixa_carta_da_mao(self) -> Carta:
-        pass#implementar
+        carta = random.choice(self.mao)
+        self.mao.remove(carta)
+        return carta
 
     '''
     @return Retorna a mao atual do jogador (lista das cartas que ele possui)
     '''
     @property
     def mao(self) -> list:
-        pass#implementar
+        return self.__mao
 
     '''
     Inclui na mao do jogador a carta passada como parametro
     @param carta Carta que sera incluida na mao do jogador
     '''
     def inclui_carta_na_mao(self, carta:Carta):
-        pass#implementar
+        self.mao.append(carta)
